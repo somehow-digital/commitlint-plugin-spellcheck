022b16dc8c9f7cdcf0fa30ab52e8bb6890b5f1b4
Thomas Deinhamer <code@thasmo.dev>
2024-09-27 14:42:08 +0200

    Use shared commitlint config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,13 +1,6 @@
 import plugin from './src/index.js';
 
 export default {
+	extends: ['@somehow-digital/commitlint-config'],
 	plugins: [plugin],
-	rules: {
-		'spellcheck/body': [2, 'always'],
-		'spellcheck/footer': [2, 'always'],
-		'spellcheck/header': [2, 'always'],
-		'spellcheck/scope': [2, 'always'],
-		'spellcheck/subject': [2, 'always'],
-		'spellcheck/type': [2, 'always'],
-	},
 };
