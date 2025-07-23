4d363067cc0363662875338780988d81af2ab2c5
Thomas Deinhamer <code@thasmo.dev>
2025-07-23 08:58:25 +0200

    Improve spellcheck error messages

--- src/index.js
+++ src/index.js
@@ -3,49 +3,52 @@ import { spellCheckDocument } from 'cspell-lib';
 async function check(text) {
 	const result = await spellCheckDocument(
 		{
-			languageId: 'plaintext',
-			locale: 'en',
 			text,
 			uri: '',
+			languageId: 'plaintext',
+			locale: 'en',
 		},
 		{},
 		{},
 	);
 
-	return result.issues.length === 0;
+	return {
+		result: result.issues.length === 0,
+		words: result.issues.map(issue => issue.text),
+	};
 }
 
 export default {
 	rules: {
-		'spellcheck/body': async ({ body }) => {
-			const result = await check(body);
+		'spellcheck/header': async ({ header }) => {
+			const { result, words } = await check(header);
 
-			return [result, 'body may not be misspelled'];
+			return [result, `header may not be misspelled: ${words.join(', ')}`];
+		},
+		'spellcheck/body': async ({ body }) => {
+			const { result, words } = await check(body);
+
+			return [result, `body may not be misspelled: ${words.join(', ')}`];
 		},
 		'spellcheck/footer': async ({ footer }) => {
-			const result = await check(footer);
+			const { result, words } = await check(footer);
 
-			return [result, 'footer may not be misspelled'];
-		},
-		'spellcheck/header': async ({ header }) => {
-			const result = await check(header);
-
-			return [result, 'header may not be misspelled'];
+			return [result, `footer may not be misspelled: ${words.join(', ')}`];
 		},
 		'spellcheck/scope': async ({ scope }) => {
-			const result = await check(scope);
+			const { result, words } = await check(scope);
 
-			return [result, 'scope may not be misspelled'];
+			return [result, `scope may not be misspelled: ${words.join(', ')}`];
 		},
 		'spellcheck/subject': async ({ subject }) => {
-			const result = await check(subject);
+			const { result, words } = await check(subject);
 
-			return [result, 'subject may not be misspelled'];
+			return [result, `subject may not be misspelled: ${words.join(', ')}`];
 		},
 		'spellcheck/type': async ({ type }) => {
-			const result = await check(type);
+			const { result, words } = await check(type);
 
-			return [result, 'type may not be misspelled'];
+			return [result, `type may not be misspelled: ${words.join(', ')}`];
 		},
 	},
 };
